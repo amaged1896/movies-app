022eabdd7ebf5732c6200b7bfff67dbe1636fe86
amaged1896 <ahmed.maged1896@gmail.com>
2023-04-12 05:46:38 +0200

    "loading screen"

---
+++ src/Components/Loading/Loading.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Loading() {
+    return (
+        <>
+
+            <div id="loading-wrapper">
+                <div id="loading-text">LOADING</div>
+                <div id="loading-content"></div>
+            </div>
+
+        </>
+    );
+}
+
+export default Loading;
